e98ba6e466c63b7a2e26892845059bc0ff8805d6
LiBai666 <overwall2016@gmail.com>
2018-07-16 12:55:24 +0800

    style(eslint): fix global return
    https://github.com/eslint/eslint/issues/8767.
    Can not use use return statement in global, use
    process.exit(0)

--- bin/clone.js
+++ bin/clone.js
@@ -4,21 +4,22 @@ const path = require('path')
 const argv = require('yargs').argv
 
 if (argv['v']) {
-  return console.log(require('../package.json').version)
+  console.log(require('../package.json').version)
+  process.exit(0)
 }
 
 if (argv['_'].length === 0 || argv['h'] || argv['help']) {
-    fs.createReadStream(path.join(__dirname, 'usage.txt')).pipe(process.stdout);
-    return null
+  fs.createReadStream(path.join(__dirname, 'usage.txt')).pipe(process.stdout)
+  process.exit(0)
 }
 
 const github = require('octonode')
 const downloadDir = require('../lib/downloadDir')
 
 let token = argv['t'] || argv['token']
-if(!token){
-	console.log('unauthorization user limte to github api 60/pre_hour')
-	token = 'be7f6099cba4524c4208d99e89f75a719346dd3b'
+if (!token) {
+  console.log('unauthorization user limte to github api 60/pre_hour')
+  token = 'be7f6099cba4524c4208d99e89f75a719346dd3b'
 }
 const client = github.client(token)
 
@@ -28,6 +29,6 @@ let fileDepth = argv['f'] || 1
 let repo = require('../lib/getRepo.js')(argv['_'][0])
 
 let url = 'https://api.github.com/repos/' + repo + '/contents'
-let repoName = repo.split("/")[1]
+let repoName = repo.split('/')[1]
 let dirLocation = path.normalize(argv['_'][1] || `./${repoName}`)
 downloadDir(client, dirLocation, url, dirDepth, fileDepth)
